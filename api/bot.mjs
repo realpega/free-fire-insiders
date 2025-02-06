0ec279d06f164a80d2a5b38cb4cc592d27047f1e
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2025-02-06 12:39:08 +0530

    Update bot.mjs

--- api/bot.mjs
+++ api/bot.mjs
@@ -4,12 +4,19 @@ const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(TOKEN);
 
 const responses = {
-  "sudo apt update": "apt updated",
+  "sudo apt update": "Hit:1 http://archive.ubuntu.com/ubuntu focal InRelease  
+Get:2 http://security.ubuntu.com/ubuntu focal-security InRelease [114 kB]  
+Get:3 http://archive.ubuntu.com/ubuntu focal-updates InRelease [114 kB]  
+Get:4 http://archive.ubuntu.com/ubuntu focal-backports InRelease [108 kB]  
+Reading package lists... Done  
+Building dependency tree       
+Reading state information... Done  
+All packages are up to date.  
+  ",
   "uname -m": "aarch64",
   "ls": "Documents  Downloads  Music  Pictures  Videos",
 };
 
-// Webhook handler
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { message } = req.body;
