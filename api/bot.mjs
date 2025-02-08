7abdf89d9dcac53d682423102c71fe918ce8f9c4
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2025-02-08 22:25:43 +0530

    Add files via upload

--- api/bot.mjs
+++ api/bot.mjs
@@ -4,46 +4,7 @@ const bot = new TelegramBot(TOKEN);
 let packagesUpgraded = false;
 const staticResponses = {
   "uname -m": "amd64",
-  "ls": "Documents  Downloads  Music  Pictures  Videos",
-  "neofetch": `
-            .-/+oossssoo+/-.               
-        \`:+ssssssssssssssssss+:\`           
-      -+ssssssssssssssssssyyssss+-         
-    .ossssssssssssssssssdMMMNysssso.       
-   /ssssssssssshdmmNNmmyNMMMMhssssss/      
-  +ssssssssshmydMMMMMMMNddddyssssssss+     
- /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/    
-.ssssssssdMMMNhsssssssssshNMMMdssssssss.   
-+sssshhhyNMMNyssssssssssssyNMMMysssssss+   
-ossyNMMMNyMMhsssssssssssssshmmmhssssssso   
-+sssshhhyNMMNyssssssssssssyNMMMysssssss+   
-.ssssssssdMMMNhsssssssssshNMMMdssssssss.   
- /sssssssshNMMMyhhyyyyhdNMMMNhssssssss/    
-  +ssssssssshmmNNMMMNNmmdhyyyyssssssss+     
-   /sssssssssshdmmNNmmyNMMMMhssssss/      
-    .ossssssssssssssssdMMMNysssso.       
-      -+ssssssssssssssssssyyssss+-         
-        \`:+ssssssssssssssssss+:\`           
-            .-/+oossssoo+/-.               
-
-root@freefireinsiders
----------------- 
-OS: Ubuntu 20.04.6 LTS x86_64  
-Host: HP Pavilion Gaming Laptop 15-ec2150AX  
-Kernel: 5.15.0-91-generic  
-Uptime: 5 hours, 42 mins  
-Packages: 1982 (dpkg), 14 (snap)  
-Shell: bash 5.0.17  
-Resolution: 1920x1080  
-DE: GNOME 3.36.9  
-WM: Mutter  
-WM Theme: Yaru  
-Theme: Yaru-dark [GTK2/3]  
-Icons: Yaru [GTK2/3]  
-Terminal: gnome-terminal  
-CPU: AMD Ryzen 5 5600H (12) @ 4.20GHz  
-GPU: NVIDIA GeForce GTX 1650 Mobile / AMD Radeon Vega 7  
-Memory: 7.8GiB / 15.5GiB`
+  "ls": "Documents  Downloads  Music  Pictures  Videos"
 };
 const lastCommand = new Map();
 const messageHistory = new Map();
